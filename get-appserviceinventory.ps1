3f7bf0c1aa30aab16b271160910deac3ca22e2ab
johnsblevins <johnsblevins@gmail.com>
2019-09-26 12:10:35 -0400

    Reordered Fields in output

--- get-appserviceinventory.ps1
+++ get-appserviceinventory.ps1
@@ -16,19 +16,20 @@ foreach ($sub in $subs)
         $appResourceGroup = $app.ResourceGroup
         $appServerFarm = ( ( $app.ServerFarmId -split '/')[-1] )
 
-        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name Name -Value $app.Name
-        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name ResourceGroup -Value $appResourceGroup
-        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name AppPlan_Id -Value $app.ServerFarmId
-        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name AppPlan_Name -Value $appServerFarm
-        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name Kind -Value $app.Kind
-        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name Location -Value $app.Location
-        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name DefaultHostName -Value $app.DefaultHostName
-        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name EnabledHostNames -Value ([string] $app.EnabledHostNames )
-        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name HostNames -Value ([string] $app.HostNames )
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name App_Id -Value $app.Id
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name App_Name -Value $app.Name
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name App_ResourceGroup -Value $appResourceGroup        
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name App_Kind -Value $app.Kind
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name App_Location -Value $app.Location
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name App_DefaultHostName -Value $app.DefaultHostName
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name App_EnabledHostNames -Value ([string] $app.EnabledHostNames )
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name App_HostNames -Value ([string] $app.HostNames )
 
         $appplan = Get-AzAppServicePlan -ResourceGroupName $app.ResourceGroup -Name $appServerFarm
-        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name AppPlan_MaxNumWorkers -Value ($appplan.MaximumNumberOfWorkers)
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name AppPlan_Id -Value $app.ServerFarmId
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name AppPlan_Name -Value $appServerFarm
         Add-Member -InputObject $appinfo -MemberType NoteProperty -Name AppPlan_ResourceGroup  -Value ($appplan.ResourceGroup)
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name AppPlan_MaxNumWorkers -Value ($appplan.MaximumNumberOfWorkers)
         Add-Member -InputObject $appinfo -MemberType NoteProperty -Name AppPlan_NumberOfSites  -Value ($appplan.NumberOfSites)
         Add-Member -InputObject $appinfo -MemberType NoteProperty -Name AppPlan_Kind  -Value ($appplan.Kind)
         Add-Member -InputObject $appinfo -MemberType NoteProperty -Name AppPlan_SKU_Name -Value ($appplan.sku.Name)
@@ -40,13 +41,13 @@ foreach ($sub in $subs)
         $ASE_Profile = $appplan.HostingEnvironmentProfile
 
         if ($ASE_Profile)
-        {
-            Add-Member -InputObject $appinfo -MemberType NoteProperty -Name ASE_Name -Value ($ASE_Profile.Name)
-            Add-Member -InputObject $appinfo -MemberType NoteProperty -Name ASE_ID -Value ($ASE_Profile.Id)            
+        {            
+            Add-Member -InputObject $appinfo -MemberType NoteProperty -Name ASE_ID -Value ($ASE_Profile.Id)   
+            Add-Member -InputObject $appinfo -MemberType NoteProperty -Name ASE_Name -Value ($ASE_Profile.Name)                     
         }
         else {
-            Add-Member -InputObject $appinfo -MemberType NoteProperty -Name ASE_Name -Value ""
             Add-Member -InputObject $appinfo -MemberType NoteProperty -Name ASE_ID -Value ""
+            Add-Member -InputObject $appinfo -MemberType NoteProperty -Name ASE_Name -Value ""            
         }
 
     }
