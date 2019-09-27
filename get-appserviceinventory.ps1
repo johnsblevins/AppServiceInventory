810492d0cc36a770e3133630be3b85189f710a2f
johnsblevins <johnsblevins@gmail.com>
2019-09-27 10:50:04 -0400

    Added Subscription Info

--- get-appserviceinventory.ps1
+++ get-appserviceinventory.ps1
@@ -18,6 +18,9 @@ foreach ($sub in $subs)
 
         Write-Output "Processing: $($app.name)"
 
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name Subscription_Id -Value $sub.Id
+        Add-Member -InputObject $appinfo -MemberType NoteProperty -Name Subscription_Name -Value $sub.Name
+
         Add-Member -InputObject $appinfo -MemberType NoteProperty -Name App_Id -Value $app.Id
         Add-Member -InputObject $appinfo -MemberType NoteProperty -Name App_Name -Value $app.Name
         Add-Member -InputObject $appinfo -MemberType NoteProperty -Name App_ResourceGroup -Value $appResourceGroup        
